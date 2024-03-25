529c1a16d0e19ce8b1bab30ae916da25a3ae6757
Brandon Boras <brandon.boras@mymail.champlain.edu>
2024-03-24 20:30:44 -0400

    initial implemenation of a fading system that is currently only applied to a general fade from black at the beginning

--- Assets/Scripts/UI/UISystem.cs
+++ Assets/Scripts/UI/UISystem.cs
@@ -1,18 +1,48 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// initial rough implementation of a fading system, going to be generalized beyond this single implementation
+
 public class UISystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float FadeTimeSeconds;
+    [SerializeField] UnityEngine.UI.Image blackCoverImage;
+
+    bool isCurrentlyFading = false;
+    float currentFadeTime = 0.0f;
+
     void Start()
     {
-        
+        StartFading(FadeTimeSeconds, blackCoverImage);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isCurrentlyFading)
+        {
+            HandleFading();
+        }
+    }
+
+    void StartFading(float timeToFullFadeSeconds, UnityEngine.UI.Image targetImage)
+    {
+        isCurrentlyFading = true;
+        currentFadeTime = FadeTimeSeconds;
+        targetImage.color = new Color(0, 0, 0, 1);
+    }
+
+    void HandleFading()
+    {
+        if (currentFadeTime > 0)
+        {
+            currentFadeTime -= Time.deltaTime;
+            blackCoverImage.color = new Color(0, 0, 0, Mathf.Lerp(1, 0, 1 - (currentFadeTime / FadeTimeSeconds)));
+        }
+        else
+        {
+            blackCoverImage.color = new Color(0, 0, 0, 0);
+            isCurrentlyFading = false;
+        }
     }
 }
