b1b16e37338231fd4b485b56d7d0e840dafd65b8
Brandon Boras <brandon.boras@mymail.champlain.edu>
2024-07-28 19:06:48 -0700

    cleaning up the example snippet

--- Assets/Scripts/BehaviorDecisionSystem/Snippets/BSnip_HandleSleep.cs
+++ Assets/Scripts/BehaviorDecisionSystem/Snippets/BSnip_HandleSleep.cs
@@ -21,6 +21,7 @@ public class BSnip_HandleSleep : BehaviorSnippet
         }
         else
         {
+            // Distance between character and their hammock
             if ((Vector2.Distance(charStats.Hammock.transform.position, charStats.gameObject.transform.position) < 0.2f))
             {
                 Debug.Log("IN BED");
@@ -36,6 +37,7 @@ public class BSnip_HandleSleep : BehaviorSnippet
 
 
     }
+
     public override void SnippetUpdate()
     {
         actionValue = ((1.0f - charStats.energy) * 125);
